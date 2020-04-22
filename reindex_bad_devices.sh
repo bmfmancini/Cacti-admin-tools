60ab41b8c97c4675d0fcf5f3b9acccdb343325eb
root <root@cacti-ebu-lab-main-ym.mgmt.net.rogers.com>
2020-04-22 10:54:46 -0400

    added commentes

--- reindex_bad_devices.sh
+++ reindex_bad_devices.sh
@@ -1,3 +1,11 @@
+##Script by Sean Mancini
+
+####This is a quick script that allow you to specify multiple devices that need to be reindex
+##when you get a messege in the log like You have 2 Devices with bad SNMP Indexes you can try this script to 
+##reindex the devices all you need is the device ID enter like so ./reindex 1 2 3
+###
+
+
 #!/bin/bash
 for id in "$@"
 do
