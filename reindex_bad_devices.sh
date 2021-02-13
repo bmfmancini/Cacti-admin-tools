3d5b63a44be3bbe4f4134bd7bc0c7c0e19300e7c
bmfmancini <sean@seanmancini.com>
2021-02-13 16:05:47 -0500

    update license

--- reindex_bad_devices.sh
+++ reindex_bad_devices.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
-##Script by Sean Mancini
 
-####This is a quick script that allow you to specify multiple devices that need to be reindex
-##when you get a messege in the log like You have 2 Devices with bad SNMP Indexes you can try this script to 
+##Author Sean Mancini
+#www.seanmancini.com
+#Version 1.0
 
+
+##Dont forget to checkout cacti @ www.cacti.net
+
+#    This program is free software: you can redistribute it and/or modify#
+#    it under the terms of the GNU General Public License as published by#
+#    the Free Software Foundation, either version 3 of the License, or#
+#    (at your option) any later version.#
+#    This program is distributed in the hope that it will be useful,#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of#
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the#
+#    GNU General Public License for more details.#
+#    You should have received a copy of the GNU General Public License#
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.#
 
 
 
